babf9ebfa2f15d7a9c6c5b0094626c88ac65c0bd
Jorge Díaz <jorge.diazma@sansano.usm.cl>
2020-10-11 20:06:32 -0300

    test

--- logistico/test.go
+++ logistico/test.go
@@ -1,5 +1,35 @@
 package main
-import "fmt"
+
+import(
+  "fmt"
+  "time"
+  "os"
+  )
+
+  type orden struct {
+      created_time Timestamp
+      id_paquete string
+      tipo string
+      nombre string
+      valor  int
+      origen string
+      destino string
+      seguimiento string
+
+  }
+
+
+func NewOrden( id_paquete string, tipo string, nombre string,
+  valor  int, origen string, destino string ) *orden {
+    orden := orden{id_paquete: name,tipo:tipo,nombre:nombre,valor:valor,
+    origen:origen,destino:destino}
+    orden.created_time = time.Now()
+    orden.seguimiento = "000001"
+    return &orden
+}
+
+
 func main() {
     fmt.Println("Wena profe")
+    fmt.Println(NewOrden("1a1a1a1","gg","Jorgekun",1000,"chilito","membrillo"))
 }
