7a2ecc1251767e111810a4d0019938b169196463
Jorge Díaz <jorge.diazma@sansano.usm.cl>
2020-10-11 20:22:12 -0300

    time

--- logistico/test.go
+++ logistico/test.go
@@ -3,11 +3,10 @@ package main
 import(
   "fmt"
   "time"
-  "os"
   )
 
   type orden struct {
-      created_time Timestamp
+      created_time time.Time 'bson:"updated_at,omitempty" json:"updated_at,omitempty"
       id_paquete string
       tipo string
       nombre string
@@ -15,13 +14,11 @@ import(
       origen string
       destino string
       seguimiento string
-
   }
-
 
 func NewOrden( id_paquete string, tipo string, nombre string,
   valor  int, origen string, destino string ) *orden {
-    orden := orden{id_paquete: name,tipo:tipo,nombre:nombre,valor:valor,
+    orden := orden{id_paquete: id_paquete,tipo:tipo,nombre:nombre,valor:valor,
     origen:origen,destino:destino}
     orden.created_time = time.Now()
     orden.seguimiento = "000001"
