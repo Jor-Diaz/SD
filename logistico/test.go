df36d54802bc18c2a1cfd4d4afadaddbb6e18e41
Jorge Díaz <jorge.diazma@sansano.usm.cl>
2020-10-11 21:30:45 -0300

    todo check

--- logistico/test.go
+++ logistico/test.go
@@ -13,26 +13,32 @@ import(
       valor  int
       origen string
       destino string
-      seguimiento string
+      seguimiento int
   }
 
-func NewOrden( id_paquete string, tipo string, nombre string,
+func NewOrden(ordenes []*orden, id_paquete string, tipo string, nombre string,
   valor  int, origen string, destino string ) *orden {
     orden := orden{id_paquete: id_paquete,tipo:tipo,nombre:nombre,valor:valor,
     origen:origen,destino:destino}
     orden.created_time = time.Now()
-    orden.seguimiento = "000001"
+    orden.seguimiento = NewCodeSeguimiento(ordenes) 
     return &orden
 }
 
+func NewCodeSeguimiento(ordenes []*orden) int{
+    if len(ordenes)==0 {
+      return 1
+    }
+    return ordenes[-1].seguimiento+1
+}
 
 
 func main() {
     fmt.Println("Wena profe")
     ordenes := []*orden{}
-    aux:=NewOrden("Paquete1","mochila","Jorgekun",1000,"chilito","membrillo")
+    aux:=NewOrden(ordenes,"Paquete1","mochila","Jorgekun",1000,"chilito","membrillo")
     ordenes=append(ordenes,aux)
-    aux=NewOrden("Paquete2","Bebida","Iñakikun",2000,"chilito","Corea")
+    aux=NewOrden(ordenes,"Paquete2","Bebida","Iñakikun",2000,"chilito","Corea")
     ordenes=append(ordenes,aux)
     for i := 0; i < len(ordenes); i++ {
       fmt.Println(ordenes[i])
