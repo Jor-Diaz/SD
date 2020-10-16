92bb15d6d29fd74b4adff516af39ea68925a52d4
jorgeSamurSansur <jorge.samur@sansano.usm.cl>
2020-10-16 15:47:24 -0300

    Arreglos a camiones

--- camiones/truck.go
+++ camiones/truck.go
@@ -11,7 +11,7 @@ import (
 	"math/rand"
 )
 /************paquetes tipos*************/
-// 0: prioritario , 1:retail ,2: normal
+// 0: retail , 1: prioritario ,2: normal
 /**************************************/
 
 /************camiones tipos*************/
@@ -30,13 +30,13 @@ type pack struct{
 
 type truck struct{
   type_t int
-  pack1 *pack
-	pack2 *pack
+  pack0 *pack
+	pack1 *pack
 }
 
 
-var pack404 pack = pack{id_pack: "not_found",pack_type: 3, value: -1, origin:  "not_found", destination:  "not_found", tries :-1, delivery_date:  time.Now()}
-var camion404 truck = truck{type_t: -1 , pack1: &pack404, pack2: &pack404 }
+var pack404 pack = pack{id_pack: "empty",pack_type: 3, value: -1, origin:  "empty", destination:  "empty", tries :-1, delivery_date:  time.Now()}
+var camion404 truck = truck{type_t: -1 , pack0: &pack404, pack1: &pack404 }
 
 /*************************************************************************************************************************************************/
 /* Función apra crear un nuevo paquete
@@ -71,8 +71,8 @@ func newPack(idPack string, typ int, val string, org string, dst string, trs int
 func newTruck(typ int, packA  *pack, packB *pack ) *truck  {
 	nTruck := truck{
 		type_t:  typ,
-		pack1 : packA,
-		pack2 : packB	}
+		pack0 : packA,
+		pack1 : packB	}
 	return &nTruck
 }
 /*************************************************************************************************************************************************/
@@ -92,14 +92,30 @@ func chanceToDeliver() int{
 /*************************************************************************************************************************************************/
 
 /*************************************************************************************************************************************************/
+/**/
+
+func truckState(trucky 	*truck) int{
+	if trucky.pack0.id_pack == "empty" && trucky.pack1.id_pack == "empty"{
+		return 0
+	} else if (trucky.pack0.id_pack != "empty" && trucky.pack1.id_pack == "empty") || (trucky.pack0.id_pack == "empty" && trucky.pack1.id_pack != "empty"){
+		return 1
+	} else{
+		return 2
+	}
+
+}
+/*************************************************************************************************************************************************/
+
+
+/*************************************************************************************************************************************************/
 /* Función que indica que paquete entregar en esta iteración
 Primero considera su prioridad
 SI son iguales prioriza por su valor
 de no existir un paquete este ya habra sido reemplazado por el paquete404
 de estar vacío retorna -1
 en la estructura de camión:
-si se retorna 1 implica que se entrega .pack2
-si se retorna 0 implica que se entrega .pack1
+si se retorna 1 implica que se entrega .pack1
+si se retorna 0 implica que se entrega .pack0
 
 */
 func wichToDeliver(pack0 *pack, pack1 *pack) int {
@@ -138,35 +154,33 @@ func deliver (pck *pack) *pack {
 /*************************************************************************************************************************************************/
 /* Función que hace la entrega de los paquetes, esta debe ser usada en un loop o directamente 2 veces
 
-
-
 */
 
 func delivery(deliver_truck *truck) *truck {
-	packToDeliver := wichToDeliver(deliver_truck.pack1, deliver_truck.pack2)
+	packToDeliver := wichToDeliver(deliver_truck.pack0, deliver_truck.pack1)
 	if packToDeliver == 0{
+		for deliver_truck.pack0.tries < 3{
+			if chanceToDeliver() == 1{
+				fmt.Println(".:| Entregando |:. -> ", deliver_truck.pack0.id_pack)
+				deliver_truck.pack0 = deliver(deliver_truck.pack0)
+				return deliver_truck
+
+			} else{
+				fmt.Println("Nuevo Intento de Entrega de ", deliver_truck.pack1.id_pack)
+
+				deliver_truck.pack0.tries++
+			}
+		}
+		return deliver_truck
+	}else if packToDeliver == 1{
 		for deliver_truck.pack1.tries < 3{
 			if chanceToDeliver() == 1{
 				fmt.Println(".:| Entregando |:. -> ", deliver_truck.pack1.id_pack)
 				deliver_truck.pack1 = deliver(deliver_truck.pack1)
 				return deliver_truck
-
-			} else{
-				fmt.Println("Nuevo Intento de Entrega de ", deliver_truck.pack2.id_pack)
-
-				deliver_truck.pack1.tries++
-			}
-		}
-		return deliver_truck
-	}else if packToDeliver == 1{
-		for deliver_truck.pack2.tries < 3{
-			if chanceToDeliver() == 1{
-				fmt.Println(".:| Entregando |:. -> ", deliver_truck.pack2.id_pack)
-				deliver_truck.pack2 = deliver(deliver_truck.pack2)
-				return deliver_truck
 			}else{
-				fmt.Println("Nuevo Intento de Entrega de ", deliver_truck.pack2.id_pack)
-				deliver_truck.pack2.tries++
+				fmt.Println("Nuevo Intento de Entrega de ", deliver_truck.pack1.id_pack)
+				deliver_truck.pack1.tries++
 			}
 		}
 		return deliver_truck
@@ -182,42 +196,57 @@ func delivery(deliver_truck *truck) *truck {
 
 func main()  {
 
-  p1 := newPack("SA5897AS", 1,"95","tienda-A","casa-D",  0,  time.Now())
-	p2 := newPack("SA6947GH", 0,"50","tienda-C","casa-C",  0,  time.Now())
+  p1 := newPack("SA5897AS", 1, "95", "_","_",  0,  time.Now())
+	p2 := newPack("SA6947GH", 0, "50", "_","_",  0,  time.Now())
+	// p3 := newPack("SA2589TR", 2, "5",  "_", "_", 0,  time.Now())
+	// p4 := newPack("SA1597EF", 0, "20", "_", "_", 0,  time.Now())
+	// p5 := newPack("SA6947GH", 1, "50", "_", "_", 0,  time.Now())
+	// p6 := newPack("SA2596NH", 2, "90", "_", "_", 0,  time.Now())
+
 	t1 := newTruck(1,p1,p2)
-	fmt.Println("----------------------------------")
+	state := truckState(t1)
+	// t2 := newTruck(0,p3,p4)
+	// t3 := newTruck(0,p5,p6)
+	fmt.Println("-----------------------------------")
 
 	fmt.Println("Antes de Entregar:")
 	fmt.Println("_Camión_")
-	fmt.Println("Paquete 1: *", t1.pack1.id_pack," *")
-	fmt.Println("Paquete 2: *", t1.pack2.id_pack," *")
-	fmt.Println("----------------------------------")
+	fmt.Println("Capacidad del Camión:", state, "espacios")
+
+	fmt.Println("Paquete 1: *", t1.pack0.id_pack," *")
+	fmt.Println("Paquete 2: *", t1.pack1.id_pack," *")
+	fmt.Println("------------------------------------")
 
 
 
-	// a := wichToDeliver(t1.pack1, t1.pack2)
+	// a := wichToDeliver(t1.pack0, t1.pack1)
 	// b:= chanceToDeliver()
 	// fmt.Println(a)
 	// fmt.Println("chance:", b)
 	// p1 = deliver(p1)
 	//fmt.Println(p1.id_pack)
 	t1 = delivery(t1)
-	fmt.Println("----------------------------------")
+	fmt.Println("-----------------------------------")
+	state = truckState(t1)
 
 	fmt.Println("Despues de 1er Entrega:")
 	fmt.Println("_Camión_")
-	fmt.Println("Paquete 1: *", t1.pack1.id_pack," *")
-	fmt.Println("Paquete 2: *", t1.pack2.id_pack," *")
-	fmt.Println("----------------------------------")
+	fmt.Println("Capacidad del Camión:", state, "espacios")
+
+	fmt.Println("Paquete 1: *", t1.pack0.id_pack," *")
+	fmt.Println("Paquete 2: *", t1.pack1.id_pack," *")
+	fmt.Println("-----------------------------------")
 
 	t1 = delivery(t1)
-	fmt.Println("----------------------------------")
-
+	fmt.Println("-----------------------------------")
+	state = truckState(t1)
 	fmt.Println("Despues de 2da Entrega:")
 	fmt.Println("_Camión_")
-	fmt.Println("Paquete 1: *", t1.pack1.id_pack," *")
-	fmt.Println("Paquete 2: *", t1.pack2.id_pack," *")
-	fmt.Println("----------------------------------")
+	fmt.Println("Capacidad del Camión:", state, "espacios")
+
+	fmt.Println("Paquete 1: *", t1.pack0.id_pack," *")
+	fmt.Println("Paquete 2: *", t1.pack1.id_pack," *")
+	fmt.Println("-----------------------------------")
 
 
 
