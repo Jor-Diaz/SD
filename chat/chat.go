7af269e813a8ad7cf6dac9f1d1f2dedb6d86cdf3
Jorge Díaz <jorge.diazma@sansano.usm.cl>
2020-10-17 19:24:23 -0300

    cla

--- chat/chat.go
+++ chat/chat.go
@@ -11,5 +11,5 @@ type Server struct {
 
 func (s *Server) SayHello(ctx context.Context, in *Message) (*Message, error) {
 	log.Printf("Orden recibida con datos:  %s %s %s %d %s %s", in.tipo,in.id,in.producto,in.valor,in.tienda,in.destino )
-	return &Message{producto: " Datos recibidos",}, nil
+	return &Message{tipo: " Datos recibidos",}, nil
 }
