b04256f9c224b91eebb1489c2a56bbe02857602d
Jorge Díaz <jorge.diazma@sansano.usm.cl>
2020-10-17 03:31:57 -0300

    cambios

--- logistico/test.go
+++ logistico/test.go
@@ -44,18 +44,22 @@ func NewCodeSeguimiento(ordenes []*orden) int{
 
 
 func main() {
-    lis, err := net.Listen("tcp", ":9000")
-    if err != nil {
-      log.Fatalf("failed to listen: %v", err)
-    }
-    grpcServer := grpc.NewServer()
-    if err := grpcServer.Serve(lis); err != nil {
-      log.Fatalf("failed to serve: %s", err)
-    }
-    chat.RegisterChatServiceServer(grpcServer, &s)
-    if err := grpcServer.Serve(lis); err != nil {
-      log.Fatalf("failed to serve: %s", err)
-    }
+    fmt.Println("Go gRPC Beginners Tutorial!")
+
+  	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 9000))
+  	if err != nil {
+  		log.Fatalf("failed to listen: %v", err)
+  	}
+
+  	s := chat.Server{}
+
+  	grpcServer := grpc.NewServer()
+
+  	chat.RegisterChatServiceServer(grpcServer, &s)
+
+  	if err := grpcServer.Serve(lis); err != nil {
+  		log.Fatalf("failed to serve: %s", err)
+  	}
     fmt.Println("Wena profe")
     ordenes := []*orden{}
     aux:=NewOrden(ordenes,"Paquete1","mochila","Jorgekun",1000,"chilito","membrillo")
