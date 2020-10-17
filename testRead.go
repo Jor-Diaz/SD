8717ac166b173fd87ed1d37260778bd8e9379a25
Jorge Díaz <jorge.diazma@sansano.usm.cl>
2020-10-17 03:39:56 -0300

    bla

--- testRead.go
+++ testRead.go
@@ -116,21 +116,19 @@ func main() {
   // Set up a connection to the server.
 
   var conn *grpc.ClientConn
-  conn, err := grpc.Dial(address, grpc.WithInsecure())
-  if err != nil {
-    log.Fatalf("did not connect: %s", err)
-  }else{
-    log.Fatalf("soy crack")
-  }
-  defer conn.Close()
-  c := chat.NewChatServiceClient(conn)
+  	conn, err := grpc.Dial(":9000", grpc.WithInsecure())
+  	if err != nil {
+  		log.Fatalf("did not connect: %s", err)
+  	}
+  	defer conn.Close()
 
-	response, err := c.SayHello(context.Background(), &chat.Message{Body: "Hello From Client!"})
-	if err != nil {
-		log.Fatalf("Error when calling SayHello: %s", err)
-	}
-	log.Printf("Response from server: %s", response.Body)
+  	c := chat.NewChatServiceClient(conn)
 
+  	response, err := c.SayHello(context.Background(), &chat.Message{Body: "Hello From Client!"})
+  	if err != nil {
+  		log.Fatalf("Error when calling SayHello: %s", err)
+  	}
+  	log.Printf("Response from server: %s", response.Body)
 
   //Función para crear el array de estructuras Item
   RetailReader() //working!
