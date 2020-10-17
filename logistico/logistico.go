5c54ebbbc7de70b03730e7f863013719c1ac7c99
Jorge Díaz <jorge.diazma@sansano.usm.cl>
2020-10-17 20:17:26 -0300

    a

--- logistico/logistico.go
+++ logistico/logistico.go
@@ -51,11 +51,11 @@ func main() {
   		log.Fatalf("failed to listen: %v", err)
   	}
 
-  	s := chat.Server{}
+  	s := pipeline.Server{}
 
   	grpcServer := grpc.NewServer()
 
-  	chat.RegisterChatServiceServer(grpcServer, &s)
+  	pipeline.RegisterChatServiceServer(grpcServer, &s)
 
   	if err := grpcServer.Serve(lis); err != nil {
   		log.Fatalf("failed to serve: %s", err)
