f412a11ada0382082627a532e8111852b8c62b38
Jorge Díaz <jorge.diazma@sansano.usm.cl>
2020-10-17 01:32:42 -0300

    check

--- logistico/test.go
+++ logistico/test.go
@@ -4,21 +4,12 @@ import(
   "fmt"
   "time"
   "google.golang.org/grpc"
-  pb "google.golang.org/grpc/examples/helloworld/helloworld"
   )
 
   const (
   	port = ":50051"
   )
 
-  type server struct {
-  	pb.UnimplementedGreeterServer
-  }
-
-  func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
-  	log.Printf("Received: %v", in.GetName())
-  	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
-  }
 
 
   type orden struct {
@@ -50,15 +41,7 @@ func NewCodeSeguimiento(ordenes []*orden) int{
 
 
 func main() {
-    lis, err := net.Listen("tcp", port)
-    if err != nil {
-      log.Fatalf("failed to listen: %v", err)
-    }
-    s := grpc.NewServer()
-    pb.RegisterGreeterServer(s, &server{})
-    if err := s.Serve(lis); err != nil {
-      log.Fatalf("failed to serve: %v", err)
-    }
+
     fmt.Println("Wena profe")
     ordenes := []*orden{}
     aux:=NewOrden(ordenes,"Paquete1","mochila","Jorgekun",1000,"chilito","membrillo")
