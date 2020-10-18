c693128d7a6a704c4584f7e4dd98d9a45bbf5744
Jorge Díaz <jorge.diazma@sansano.usm.cl>
2020-10-17 22:54:40 -0300

    test

--- cliente/cliente.go
+++ cliente/cliente.go
@@ -61,7 +61,7 @@ func RetailReader(){
 			fmt.Println("Error ::", err)
 			break
 		}
-    prod := Item{id: record[0], producto: record[1], valor:strconv.FormatInt(record[2],10), tienda:record[3],destino:record[4]}
+    prod := Item{id: record[0], producto: record[1], valor:strconv.Atoi(record[2]), tienda:record[3],destino:record[4]}
     productos = append(productos, &prod)
   	}
 }
