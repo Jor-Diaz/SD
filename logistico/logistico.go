6e2f5325e8658b777fbce57406c4cc10ca3ae836
Jorge Díaz <jorge.diazma@sansano.usm.cl>
2020-10-18 01:34:21 -0300

    test

--- logistico/logistico.go
+++ logistico/logistico.go
@@ -22,15 +22,14 @@ import(
   func (s *Server) SayHello(ctx context.Context, in *pb.Message) (*pb.Message, error) {
   	log.Printf("Orden recibida con datos:   %s %s %d %s %s %d", in.Id,in.Producto,in.Valor,in.Tienda,in.Destino, in.Prioridad )
     aux:=NewOrden(ordenes,in.Id,in.Producto,in.Valor,in.Tienda,in.Destino,in.Prioridad)
-    ordenes=append(ordenes,aux)
-    //fmt.Println("#################")
-    //fmt.Println("_Data en memoria__")
-    //for i := 0; i < len(ordenes); i++ {
-      //fmt.Println(ordenes[i])
-      //fmt.Println(ordenes[i].created_time.Format(time.ANSIC))
-      //fmt.Println("_________________")
-    //}
+    //ordenes=append(ordenes,aux)
   	return &pb.Message{Seguimiento: aux.seguimiento,}, nil
+  }
+  func (s *Server) ConEstado(ctx context.Context, in *pb.consulta_estado) (*pb.respuesta_consulta, error) {
+  	log.Printf("Cosulta recibida con datos:   %d", in.Seguimiento)
+    orden_aux:=searchOrder(in.Seguimiento).
+  	return &pb.respuesta_consulta{id_paquete: orden_aux.id_paquete,nombre:orden_aux.nombre,valor:orden_aux.valor,
+    origen:orden_aux.origen,destino:orden_aux.destino,prioridad:orden_aux.prioridad,intentos=orden_aux.intentos,estado:orden_aux.estado}, nil
   }
 
   type orden struct {
@@ -42,14 +41,20 @@ import(
       destino string
       prioridad int32
       seguimiento int32
+      intentos int32
+      Estado string
   }
 
 func NewOrden(ordenes []*orden, id_paquete string, nombre string,
   valor  int32, origen string, destino string, prioridad int32 ) *orden {
     orden := orden{id_paquete: id_paquete,nombre:nombre,valor:valor,
-    origen:origen,destino:destino,prioridad:prioridad}
+    origen:origen,destino:destino,prioridad:prioridad,intentos:0,estado:"Pendiente Entrega"}
     orden.created_time = time.Now()
     orden.seguimiento = NewCodeSeguimiento(ordenes)
+    writer := csv.NewWriter("data.csv")
+    defer writer.Flush()
+    err := writer.Write(orden)
+    checkError("Cannot write to file", err)
     return &orden
 }
 
@@ -60,7 +65,20 @@ func NewCodeSeguimiento(ordenes []*orden) int32{
     return ordenes[len(ordenes)-1].seguimiento+1
 }
 
+func searchOrder( codigo_seguimiento int32) *Orden {
+  for _, v := range ordenes {
+    if v.seguimiento == codigo_seguimiento {
+          return v
+        }
+      }
+      //Item404 := Item{id: "not_found", producto: "not_found", valor:"not_found", tienda:"not_found",destino:"not_found"}
+      return &Orden404
+    }
+
+
 var ordenes []*orden
+var Orden404 Orden = Orden{id: "not_found", producto: "not_found", valor:1, tienda:"not_found",destino:"not_found", prioritario: -1,seguimiento:-1,estado:"No Existe"}
+
 func main() {
     fmt.Println("Gracias por iniciar el receptor de ordenes de SD X-Wing Team")
   	lis, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", 9000))
