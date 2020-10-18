74c12b09084c29b34ddea7401a147b1e27ff31eb
Jorge Díaz <jorge.diazma@sansano.usm.cl>
2020-10-18 16:12:15 -0300

    a

--- logistico/logistico.go
+++ logistico/logistico.go
@@ -128,11 +128,9 @@ var Orden404 orden = orden{id_paquete: "not_found", nombre: "not_found", valor:1
 
 func main() {
     fmt.Println("Gracias por iniciar el receptor de ordenes de SD X-Wing Team")
-    for i :=0; i<4 ;i++ {
-        can:=SafeCounter{v: make(map[string]int)}
-        candados=append(candados,can)
-    }
+    
     numero_seguimiento=0
+    candados= {SafeCounter{v: make(map[string]int)},SafeCounter{v: make(map[string]int)},SafeCounter{v: make(map[string]int)},SafeCounter{v: make(map[string]int)} }
     go recepcion_clientes()
     opcion:=0
     for opcion!=-1{
