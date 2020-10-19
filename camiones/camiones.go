38519f2520da8f317395089ced9a17ec76289d5e
Jorge Díaz <jorge.diazma@sansano.usm.cl>
2020-10-19 13:56:23 -0300

    a

--- camiones/camiones.go
+++ camiones/camiones.go
@@ -257,7 +257,9 @@ func ejecucion_camion(Id_camion int32, tiempo_espera float64){
 					log.Fatalf("Error when calling SayHello: %s", err)
 				}
 				///Verificar que es paquete valido
-				no_paquetes=1
+				if (response.Prioridad!=-1){
+					no_paquetes=1
+				}
 				if (no_paquetes==1){
 						status=0
 						log.Printf("Orden asignada con codigo seguimiento %d al camion %d",response.Seguimiento,Id_camion)
@@ -274,8 +276,9 @@ func ejecucion_camion(Id_camion int32, tiempo_espera float64){
 									if err != nil {
 										log.Fatalf("Error when calling SayHello: %s", err)
 									}
-									///Verificar que es paquete valido
-									no_paquetes=2
+									if (response.Prioridad!=-1){
+										no_paquetes=2
+									}									
 									if (no_paquetes==2){
 										log.Printf("Orden asignada con codigo seguimiento %d al camion %d",response.Seguimiento,Id_camion)
 										paquete_1 := newPack(response.Id, 2, response.Valor, response.Tienda,response.Destino, 0,  time.Now(),response.Seguimiento)
