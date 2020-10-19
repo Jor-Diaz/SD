db84f3ceaa470332298fe0419e1400563dfceef3
Jorge Díaz <jorge.diazma@sansano.usm.cl>
2020-10-18 22:25:28 -0300

    a

--- logistico/logistico.go
+++ logistico/logistico.go
@@ -51,6 +51,9 @@ func (s *Server) ConEstado(ctx context.Context, in *pb.ConsultaEstado) (*pb.Resp
 
 func (s *Server) Solpedido(ctx context.Context, in *pb.Solcamion) (*pb.RespuestaCon, error) {
   	log.Printf("Peticion de orden por camion de id  %d", in.IdCamion)
+    if (in.IdCamion ==1){
+
+    }
     orden_aux:=searchOrder(1)
   	return &pb.RespuestaCon{Id: orden_aux.id_paquete,Producto:orden_aux.nombre,Valor:orden_aux.valor,Tienda:orden_aux.origen,Destino:orden_aux.destino,Prioridad:orden_aux.prioridad,Intentos:orden_aux.intentos,Estado:orden_aux.estado,Seguimiento:orden_aux.seguimiento,IdCamion:orden_aux.id_camion}, nil
   }
@@ -99,6 +102,31 @@ func NewCodeSeguimiento() int32{
     candados[3].mux.Unlock()
     return aux
 }
+
+func searchOrder_retail(id_camion int32) *orden {
+  i:=0
+  for _, v := range ordenes_retail {
+    if v.estado == 0 && v.id_camion==-1  {
+          candados[0].mux.Lock()
+          ordenes_retail[i].id_camion=id_camion
+          candados[0].mux.Unlock()
+          return v
+        }
+    i=i+1
+  }
+  i=0
+  for _, v := range ordenes_prioridad_1 {
+    if v.estado == 0 && b.id_camion==-1  {
+          candados[1].mux.Lock()
+          ordenes_retail[i].id_camion=id_camion
+          candados[1].mux.Unlock()
+          return v
+        }
+    i=i+1
+  }
+  return &Orden404
+}
+
 
 func searchOrder(codigo_seguimiento int32) *orden {
   for _, v := range ordenes_retail {
